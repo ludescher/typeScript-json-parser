f6ac01760d8a94de6182eaf469240aa0ae521aa0
Johannes Ludescher <jlu@raade.at>
2021-06-25 09:54:27 +0200

    added new entry index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import parseAsClass from './dist/Parser/ClassParser';
+import ClassManager from './dist/Manager/ClassManager';
+import EntityManager from './dist/Manager/EntityManager';
+import SupportedType from './dist/Enum/SupportedType';
+import AbstractEntity from './dist/Abstract/AbstractEntity';
+
+export default {
+    parseAsClass,
+    ClassManager,
+    EntityManager,
+    SupportedType,
+    AbstractEntity,
+};
